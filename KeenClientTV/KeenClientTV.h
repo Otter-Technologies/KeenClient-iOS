9dcd61180c9bbd261a8cae8163cca528de3f8565
Pavel Hlavnicka <pavel.hlavnicka@gmail.com>
2016-12-23 16:42:09 +0100

    keen client for tvOS

---
+++ KeenClientTV/KeenClientTV.h
@@ -0,0 +1,28 @@
+//
+//  KeenClientTV.h
+//  KeenClientTV
+//
+//  Created by Pavel Hlavnicka on 20/12/2016.
+//  Copyright © 2016 Keen Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeenClientTV.
+FOUNDATION_EXPORT double KeenClientTVVersionNumber;
+
+//! Project version string for KeenClientTV.
+FOUNDATION_EXPORT const unsigned char KeenClientTVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeenClientTV/PublicHeader.h>
+
+
+#import <KeenClientTV/KeenClient.h>
+#import <KeenClientTV/KeenProperties.h>
+#import <KeenClientTV/KeenProperties.h>
+
+#import <KeenClientTV/KIODBStore.h>
+#import <KeenClientTV/KIOQuery.h>
+#import <KeenClientTV/KIOReachability.h>
+
+#import <KeenClientTV/HTTPCodes.h>
